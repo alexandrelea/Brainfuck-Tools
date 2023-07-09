9f81842b4b26cda2048c4b51497eebcd5f69d502
Water Horizontal Branch <3287597970@qq.com>
2023-07-09 12:45:29 +0800

    The 0.4 version of the program.

--- main.c
+++ main.c
@@ -19,8 +19,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+char version[10]="0.4";
 void cpr(){
-    puts("bfi (BrainFuck Interpreter) v0.3 Copyright (C) 2023 Water Horizontal Branch");
+    printf("bfi (BrainFuck Interpreter) v%s Copyright (C) 2023 Water Horizontal Branch\n",version);
     puts("This program comes with ABSOLUTELY NO WARRANTY; for details please read warranty.");
     puts("This is free software, and you are welcome to redistribute it");
     puts("under certain conditions; please read copying for details.");
@@ -35,9 +36,10 @@ int homp(){
     int choice;
     puts("What do you want to do:");
     puts("1 - Edit program");
-    puts("2 - Run program");
-    puts("3 - Debug program");
-    puts("4 - Read informations about this program.");
+    puts("2 - Analyze program");
+    puts("3 - Run program");
+    puts("4 - Debug program");
+    puts("5 - Read informations about this program.");
     puts("0 - Exit");
     printf("Please select(0-3):");
     scanf("%d",&choice);
@@ -45,28 +47,32 @@ int homp(){
 }
 char prog[100010]={};
 int len=0;
+int khop[100010]={},top,stck[100010]={},pt=0,prpt=0,analed=0;
+char mem[30010]={};
 void edit(){
     char ch,msg[100110]="Your program:";
     puts("You can copy and edit your program. The end sign is '!'.");
     if(strlen(prog)!=0) puts(strcat(msg,prog));
-    len=0;
+    analed=len=0;
     memset(prog,0,sizeof prog);
     while(scanf("%c",&ch)!=0&&ch!='!')
         if(ch=='['||ch==']'||ch=='<'||ch=='>'||ch=='+'||ch=='-'||ch=='.'||ch==',') prog[len++]=ch;
     return;
 }
-char getche(){
+char getch(){
     char c;
     system("stty -echo;stty -icanon");
     c=getchar();
     system("stty echo;stty icanon");
     return c; 
 }
-void run(){
+void anal(){
     puts("Analyzing your program...");
     len=strlen(prog);
-    int khop[100010]={},top,stck[100010]={},pt=0,prpt=0;
-    char mem[30010]={};
+    if(len==0){
+        puts("Your program is empty!");
+        return;
+    }
     for(int i=0;i<len;i++){
         if(prog[i]=='[') stck[++top]=i;
         if(prog[i]==']'){
@@ -76,15 +82,24 @@ void run(){
                 for(int j=max(0,i-10);j<i;j++) printf(" ");
                 printf("X\n");
                 printf("Extra parenthesis at %d.\n",i);
-                return;
+                puts("");
             }else khop[stck[top]]=i,khop[i]=stck[top],top--;
         }
     }
     if(top!=0){
-        printf("Missing some parentheses at %d.\n",len);
+        printf("Missing parentheses at %d.\n",len);
         return;
     }
+    puts("Analysis successful.");
+    analed=1;
+}
+void run(){
+    memset(mem,0,sizeof mem);
     getchar();
+    if(analed==0){
+        puts("Hasn't analyzed yet!");
+        return;
+    }
     for(prpt=0;prpt!=len+1;prpt++){
         switch(prog[prpt]){
         case '<':
@@ -104,7 +119,7 @@ void run(){
             fflush(stdout);
             break;
         case ',':
-            mem[pt]=getche();
+            mem[pt]=getch();
             break;
         case '[':
             if(mem[pt]=='\0') prpt=khop[prpt];
@@ -118,11 +133,114 @@ void run(){
     return;
 }
 void debug(){
-    puts("This can't be used in this program(version 0.3).");
+    memset(mem,0,sizeof mem);
+    if(analed==0){
+        puts("Hasn't analyzed yet!");
+        return;
+    }
+    char ch;
+    int mod=1;
+    puts("Auto,Fastest or Manual?(A/F/M)");
+    ch=getch();
+    while(ch!='A'&&ch!='M'&&ch!='a'&&ch!='m'&&ch!='F'&&ch!='f') puts("Please enter A or M."),ch=getch();
+    if(ch=='A'||ch=='a') mod=0;
+    if(ch=='F'||ch=='f') mod=-1;
+    pt=0;
+    int outfl=0;
+    char outed[100010]={};
+    for(prpt=0;prpt!=len+1;prpt++){
+        puts("Code area:");
+        for(int j=max(0,prpt-10);j<=min(len,prpt+10);j++) printf("%c",prog[j]);
+        printf("\n");
+        for(int j=max(0,prpt-10);j<prpt;j++) printf(" ");
+        printf("N\n");
+        puts("Memory area:");
+        for(int i=max(0,pt-10);i<=min(pt+10,30000);i++){
+            printf("%4d",mem[i]);
+            switch(mem[i]){
+            case '\0':
+                printf("\\0");
+                break;
+            case '\a':
+                printf("\\a");
+                break;
+            case '\b':
+                printf("\\b");
+                break;
+            case '\t':
+                printf("\\t");
+                break;
+            case '\n':
+                printf("\\n");
+                break;
+            case '\v':
+                printf("\\v");
+                break;
+            case '\f':
+                printf("\\f");
+                break;
+            case '\r':
+                printf("\\r");
+                break;
+            case '\\':
+                printf("\\\\");
+                break;
+            case '\'':
+                printf("\\\'");
+                break;
+            case '\"':
+                printf("\\\"");
+                break;
+            case '\?':
+                printf("\\\?");
+                break;
+            default:
+                if(mem[i]>=32) printf(" %c",mem[i]);
+                else printf("UO");
+            }
+        }
+        printf("\n");
+        for(int i=max(0,pt-10);i<pt;i++) printf("      ");
+        printf("NNNNNN\n");
+        puts("Out message:");
+        printf("%s\n",outed);
+        switch(prog[prpt]){
+        case '<':
+            pt=(pt-1+30000)%30000;
+            break;
+        case '>':
+            pt=(pt+1)%30000;
+            break;
+        case '+':
+            mem[pt]++;
+            break;
+        case '-':
+            mem[pt]--;
+            break;
+        case '.':
+            outed[outfl++]=mem[pt];
+            fflush(stdout);
+            break;
+        case ',':
+            printf("Please enter a character:");
+            ch=getch();printf("%c\n",ch);
+            break;
+        case '[':
+            if(mem[pt]=='\0') prpt=khop[prpt];
+            break;
+        case ']':
+            if(mem[pt]!='\0') prpt=khop[prpt];
+            break;
+        }
+        if(mod==-1) system("sleep 0.001");
+        else if(mod==1) getchar();
+        else system("sleep 0.1");
+    }
+    puts("");
     return;
 }
 void inform(){
-    puts("bfi - BrainFuck Interpreter by Water Horizontal Branch version 0.3");
+    printf("bfi - BrainFuck Interpreter by Water Horizontal Branch version %s\n",version);
     puts("Cell size:30000");
     puts("Cell code:ASCII(unsigned char in C)");
     puts("");
@@ -145,18 +263,21 @@ int main(){
             edit();
             break;
         case 2:
-            run();
+            anal();
             break;
         case 3:
-            debug();
+            run();
             break;
         case 4:
+            debug();
+            break;
+        case 5:
             inform();
             break;
         case 0:
             return 0;
         default:
-            puts("Sorry, this choice is incorrect.");
+            puts("This choice is incorrect.");
         }
     }
     return 0;
